1bfd5d1a15481810b61a514be37cf317394bdfa1
Maicon Wagner <mister-x59@hotmail.com.br>
2019-11-13 17:07:41 -0300

    add new ngxs-entity-state-cache

--- src/public_api.ts
+++ src/public_api.ts
@@ -5,4 +5,5 @@
 export * from './lib/ngxs-entity-state.adapter';
 export * from './lib/ngxs-entity-state.selector';
 export * from './lib/ngxs-entity-state.state.model';
+export * from './lib/ngxs-entity-state.cache';
 
